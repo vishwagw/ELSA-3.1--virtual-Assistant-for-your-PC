328235ec44e0f0be5bf1c90a0c888a7d39513c1f
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-14 08:41:32 +0530

    Create else_v3.2.2.py

---
+++ elsa3.2.2/else_v3.2.2.py
@@ -0,0 +1,38 @@
+# version 3.2.2
+# created on: 14th august 2025
+
+import speech_recognition as sr
+import datetime
+import webbrowser
+import wikipedia
+import psutil
+import os
+import subprocess
+import platform
+import pygame
+import tempfile
+import time
+import requests
+import re
+import random
+import cv2
+import numpy as np
+import pyaudio
+import plyer
+import websockets
+import asyncio
+import threading
+import json
+import pickle
+from collections import defaultdict, deque
+from typing import Dict, List, Any, Optional
+from dataclasses import dataclass, asdict
+from queue import Queue
+from ultralytics import YOLO
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+
+# importing surveillance system:
+from surv_sys import MultiSurveillanceSystem
